0dd30c512d8c6a642a6378ef210154a108b57446
Makrina Nikolopoulou <t8240094@aueb.gr>
2025-11-24 18:30:10 +0200

    database connection

---
+++ data-management/src/main/java/com/bonus71/data/EthnikiAmina.java
@@ -0,0 +1,26 @@
+package com.bonus71.data;
+public class EthnikiAmina {
+
+    private int meizona;
+    private String onomasia;
+    private String evro;
+
+    public EthnikiAmina(int meizona, String onomasia, String evro) {
+        this.meizona = meizona;
+        this.onomasia = onomasia;
+        this.evro = evro;
+    }
+
+    public int getMeizona() {
+        return meizona;
+    }
+
+    public String getOnomasia() {
+        return onomasia;
+    }
+
+    public String getEvro() {
+        return evro;
+    }
+}
+
