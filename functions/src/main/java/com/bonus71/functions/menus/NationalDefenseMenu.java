ab52a067ac2e24de3df2bd1841d9ceee84bf9c99
katerinamwraith3 <katerina.mwraithh@gmail.com>
2025-11-29 17:48:31 +0200

    delete random class

--- functions/src/main/java/com/bonus71/functions/menus/NationalDefenseMenu.java
+++ functions/src/main/java/com/bonus71/functions/menus/NationalDefenseMenu.java
@@ -1,6 +1,6 @@
 package com.bonus71.functions.menus;
 
-import com.bonus71.data.MainMenus;
+import com.bonus71.bootstrap.Main;
 import com.bonus71.data.repository.NationalDefenseRepository;
 
 import java.sql.SQLException;
@@ -8,13 +8,13 @@ import java.util.Scanner;
 
 public class NationalDefenseMenu {
     public static void menu(NationalDefenseRepository repo) throws SQLException {
-        Scanner scanner = MainMenus.getScanner();
+        Scanner scanner = Main.getScanner();
 
-        System.out.println("\n--- ΥΠΟΥΡΓΕΙΟ ΕΘΝΙΚΗΣ ΑΜΥΝΑΣ ---");
-        System.out.println("1. Προβολή");
-        System.out.println("2. Προσθήκη");
-        System.out.println("3. Ενημέρωση");
-        System.out.println("4. Διαγραφή");
+        System.out.println("\n--- NATIONAL DEFENSE  ---");
+        System.out.println("1. View");
+        System.out.println("2. Add");
+        System.out.println("3. Update");
+        System.out.println("4. Delete");
 
         int ch = scanner.nextInt();
         scanner.nextLine();
@@ -23,13 +23,13 @@ public class NationalDefenseMenu {
             case 1 -> repo.findAll().forEach(p ->
                     System.out.println(p.getMajorCategory() + " | " + p.getName() + " | " + p.getEuros()));
             case 2 -> {
-                System.out.print("Μείζονα Κατηγορία: "); int m = scanner.nextInt(); scanner.nextLine();
+                System.out.print("Code: "); int m = scanner.nextInt(); scanner.nextLine();
                 System.out.print("Ονομασία: "); String o = scanner.nextLine();
                 System.out.print("Ευρώ: "); String e = scanner.nextLine();
                 repo.insert(new com.bonus71.data.entity.ministry.NationalDefense(m, o, e));
             }
             case 3 -> {
-                System.out.print("Μείζονα Κατηγορία: "); int m = scanner.nextInt(); scanner.nextLine();
+                System.out.print("Code: "); int m = scanner.nextInt(); scanner.nextLine();
                 System.out.print("Νέα ονομασία: "); String o = scanner.nextLine();
                 System.out.print("Νέο ευρώ: "); String e = scanner.nextLine();
                 repo.update(new com.bonus71.data.entity.ministry.NationalDefense(m, o, e));
