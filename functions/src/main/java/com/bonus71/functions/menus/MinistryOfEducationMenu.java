89e20b2c26fbeabfcbf425f13be1ae4781b47200
Makrina Nikolopoulou <t8240094@aueb.gr>
2025-11-29 17:16:53 +0200

    import statement fixes

--- functions/src/main/java/com/bonus71/functions/menus/MinistryOfEducationMenu.java
+++ functions/src/main/java/com/bonus71/functions/menus/MinistryOfEducationMenu.java
@@ -1,6 +1,6 @@
 package com.bonus71.functions.menus;
 
-import com.bonus71.data.MainMenus;
+import com.bonus71.functions.MainMenus;
 import com.bonus71.data.entity.ministry.Education;
 import com.bonus71.data.repository.EducationRepository;
 
--- functions/src/main/java/com/bonus71/functions/menus/MinistryOfTHealthMenu.java
+++ functions/src/main/java/com/bonus71/functions/menus/MinistryOfTHealthMenu.java
@@ -1,6 +1,6 @@
 package com.bonus71.functions.menus;
 
-import com.bonus71.data.MainMenus;
+import com.bonus71.functions.MainMenus;
 import com.bonus71.data.entity.ministry.Health;
 import com.bonus71.data.repository.HealthRepository;
 
