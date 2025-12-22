555abe0585ba845129da7ec1ca7d4ea887631424
Makrina Nikolopoulou <t8240094@aueb.gr>
2025-12-22 15:40:12 +0200

    checkstyle fixes

--- functions/src/test/java/com/bonus71/functionstest/main/functions/test/DisplayFunctionATest.java
+++ functions/src/test/java/com/bonus71/functionstest/main/functions/test/DisplayFunctionATest.java
@@ -1,44 +1,44 @@
 package com.bonus71.functionstest.main.functions.test;
 
-import com.bonus71.functions.main.functions.DisplayFunctionA;
-import org.junit.After;
-import org.junit.Test;
-import org.junit.Before;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.bonus71.functions.main.functions.DisplayFunctionA;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.sql.SQLException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DisplayFunctionATest {
-    private final PrintStream originalOut = System.out;
-    private ByteArrayOutputStream outContent;
+  private final PrintStream originalOut = System.out;
+  private ByteArrayOutputStream outContent;
 
-    @Before
+  @Before
     public void setUp() {
-        outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-    }
+      outContent = new ByteArrayOutputStream();
+      System.setOut(new PrintStream(outContent));
+  }
 
-    @After
+  @After
     public void tearDown() {
-        System.setOut(originalOut);
-    }
+    System.setOut(originalOut);
+  }
 
-    @Test
+  @Test
     public void testShowAllRunsAndPrintsHeaders() throws SQLException {
 
 
-        DisplayFunctionA.showAll();
+    DisplayFunctionA.showAll();
 
 
-        String output = outContent.toString();
+    String output = outContent.toString();
 
 
-        assertTrue(output.contains("=== DISPLAY EXPENDITURES ==="));
-        assertTrue(output.contains("=== DISPLAY REVENUES ==="));
-        assertTrue(output.contains("=== DISPLAY ARTICLE 2 EXPENDITURES ==="));
-    }
+    assertTrue(output.contains("=== DISPLAY EXPENDITURES ==="));
+    assertTrue(output.contains("=== DISPLAY REVENUES ==="));
+    assertTrue(output.contains("=== DISPLAY ARTICLE 2 EXPENDITURES ==="));
+  }
 }
 
