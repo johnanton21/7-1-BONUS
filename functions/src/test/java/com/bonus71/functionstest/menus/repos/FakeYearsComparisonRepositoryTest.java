a964c442ba30eeb564b092f5307356a1fd959301
AlikiStamatelatou <t8240132@aueb.gr>
2025-12-24 17:57:06 +0200

    comments

--- functions/src/test/java/com/bonus71/functionstest/menus/repos/FakeYearsComparisonRepositoryTest.java
+++ functions/src/test/java/com/bonus71/functionstest/menus/repos/FakeYearsComparisonRepositoryTest.java
@@ -4,52 +4,49 @@ import com.bonus71.data.entity.Comparison.YearsComparison;
 import com.bonus71.data.repository.YearsComparisonRepository;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.jupiter.api.Test;
 
 public class FakeYearsComparisonRepositoryTest {
-
 
     static class FakeYearsComparisonRepository extends YearsComparisonRepository {
         private final Map<Integer, YearsComparison> data = new HashMap<>();
 
-    public void add(int year, YearsComparison yc) {
-      data.put(year, yc);
-    }
+        public void add(int year, YearsComparison yc) {
+            data.put(year, yc);
+        }
 
         @Override
-        public YearsComparison findByYear(int year) {
+        public YearsComparison findByYear(int year) throws SQLException {
             return data.get(year);
+        }
     }
-  }
 
     @Test
     void testFindExistingYearReturnsCorrectData() throws SQLException {
         FakeYearsComparisonRepository fakeRepo = new FakeYearsComparisonRepository();
 
-    YearsComparison yc = new YearsComparison(
+        YearsComparison yc = new YearsComparison(
                 2025, "61000000000", "57900000000", "3100000000"
-    );
+        );
 
-    fakeRepo.add(2025, yc);
+        fakeRepo.add(2025, yc);
 
-    YearsComparison result = fakeRepo.findByYear(2025);
+        YearsComparison result = fakeRepo.findByYear(2025);
 
-    assertNotNull(result);
-    assertEquals("61000000000", result.getNetRevenues());
-    assertEquals("57900000000", result.getExpenses());
-    assertEquals("3100000000", result.getBalance());
-  }
+        assertNotNull(result);
+        assertEquals("61000000000", result.getNetRevenues());
+        assertEquals("57900000000", result.getExpenses());
+        assertEquals("3100000000", result.getBalance());
+    }
 
     @Test
     void testFindMissingYearReturnsNull() throws SQLException {
         FakeYearsComparisonRepository fakeRepo = new FakeYearsComparisonRepository();
 
-    YearsComparison result = fakeRepo.findByYear(2023);
+        YearsComparison result = fakeRepo.findByYear(2023);
 
-    assertNull(result);
-  }
+        assertNull(result);
+    }
 }
