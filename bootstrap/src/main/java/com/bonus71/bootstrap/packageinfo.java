52b677a6ae8baed332e37309da4612c421722a57
katerinamwraith3 <katerina.mwraithh@gmail.com>
2026-01-10 10:59:15 +0200

    update licence

--- bootstrap/src/main/java/com/bonus71/bootstrap/packageinfo.java
+++ bootstrap/src/main/java/com/bonus71/bootstrap/packageinfo.java
@@ -1,3 +1,11 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2026 7+1 BONUS
+ *
+ * Licensed under the MIT License.
+ */
+
 /**
  * Bootstrap module of the application.
  * Contains the main enter point and coordinates all the other modules.
--- functions/src/main/java/com/bonus71/functions/menus/ExpenditureMenu.java
+++ functions/src/main/java/com/bonus71/functions/menus/ExpenditureMenu.java
@@ -1,3 +1,11 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2026 7+1 BONUS
+ *
+ * Licensed under the MIT License.
+ */
+
 package com.bonus71.functions.menus;
 
 
